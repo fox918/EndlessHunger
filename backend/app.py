a4e8c68b5646e2aa81ad49066e5788469677d51b
Oliver <oliver.wisler@endress.com>
2023-10-28 23:45:12 +0200

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 #!/bin/python3
 from flask import Flask, jsonify
 from coop_locations import getCoopLocations
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/locations')
+
 def locations():
         coopData = getCoopLocations("Basel", 10)
         return jsonify(coopData)
