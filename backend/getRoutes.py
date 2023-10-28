fe98f914b5588fe0691c8c8e30f6e81f178f13bf
Daniel Dienemann <daniel.dienemann@endress.com>
2023-10-28 22:42:00 +0200

    !!!NOT WORKING COMMIT!!!

--- backend/getRoutes.py
+++ backend/getRoutes.py
@@ -3,9 +3,9 @@ import json
 import xml.etree.ElementTree as ET# Constructing the XML elements and attributes
 from coop_locations import getCoopLocations
 
-coopLocations = getCoopLocations("Basel", 10)
+originCoordinates, coopLocations = getCoopLocations("Basel", 10)
 
-def getRoute(coopLocations, routing_profile):
+def getRoute(coopLocations, routingProfile):
     longitude = coopLocations.get("Longitude")
     latitude = coopLocations.get("Latitude")
     body = {"coordinates": [[8.681495, 49.41461], [longitude, latitude]]}
@@ -17,28 +17,31 @@ def getRoute(coopLocations, routing_profile):
     }
     # Use the routing profile variable in the URL
     
-    url = f'http://api.openrouteservice.org/v2/directions/{routing_profile}/geojson'
+    url = f'http://api.openrouteservice.org/v2/directions/{routingProfile}/geojson'
 
     call = requests.post(url, json=body, headers=headers)
 
 
     if call.status_code == 200:
-        response_json = json.loads(call.text)  # Parse the JSON response
+        route = json.loads(call.text)  # Parse the JSON response
         print("Route calculated")
-        return response_json
+        return route
     else:
         print("Request was not successful. Status code:", call.status_code)
 
+def getRouteAndData(coopLocation, routingProfile):
+    return coopLocation + getRoute(coopLocation,routingProfile)
 
-def getAllRoutes(routing_profile, coopLocations):
 
-    if routing_profile == "publicTransport":
+def getAllRoutes(routingProfile, coopLocations):
+
+    if routingProfile == "publicTransport":
         
         pass
 
-    elif routing_profile == "driving-car" or routing_profile == "cycling-regular" or routing_profile == "foot-walking" or routing_profile == "wheelchair":
+    elif routingProfile == "driving-car" or routingProfile == "cycling-regular" or routingProfile == "foot-walking" or routingProfile == "wheelchair":
 
-        routes = list(map(lambda x: getRoute(x, routing_profile), coopLocations))
+        routes = list(map(lambda x: getRouteAndData(x, routingProfile), coopLocations))
 
         with open('routes.json', 'w') as json_file:
             json.dump(routes, json_file, indent=4)  # Save the JSON to a file with indentation
@@ -53,6 +56,6 @@ def getAllRoutes(routing_profile, coopLocations):
         return sortedRoutes
 
     else:
-        print("Wrong Routing Profile", routing_profile)
+        print("Wrong Routing Profile", routingProfile)
 
 getAllRoutes("driving-car", coopLocations)
